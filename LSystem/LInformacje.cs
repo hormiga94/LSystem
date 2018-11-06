9b2eb2872aad0a1d77a30d843bfcd6ebcf62e1b5
hormiga94 <karolina.mrowa@gmail.com>
2018-11-06 18:35:40 +0100

    v 1.0

---
+++ LSystem/LInformacje.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LSystem
+{
+    class LInformacje
+    {
+        public string aksjomat { get; set; }
+
+        // REGUŁY ZAMIAN
+        public string regula1 { get; set; }
+        public string regula2 { get; set; }
+        public string regula3 { get; set; }
+        public string regula4 { get; set; }
+        public string regula5 { get; set; }
+        public string regula6 { get; set; }
+
+        // ZMIENNA DO ZAMIANY
+        public string z1 { get; set; }
+        public string z2 { get; set; }
+        public string z3 { get; set; }
+        public string z4 { get; set; }
+        public string z5 { get; set; }
+        public string z6 { get; set; }
+
+        public double kat { get; set; }
+        public double krok { get; set; }
+        public int powtorzenia { get; set; }
+
+        // PRAWDOPODOBIEŃSTWA
+        public double p1 { get; set; }
+        public double p2 { get; set; }
+        public double p3 { get; set; }
+        public double p4 { get; set; }
+        public double p5 { get; set; }
+        public double p6 { get; set; }
+
+        private Canvas myCanva= new Canvas();
+        public LInformacje()
+        {
+
+        }
+
+        public Canvas LRysowanie()
+        {
+            Point point1 = new Point(300,200);
+            Point point2 = new Point();
+
+            List<char> regulatmp = aksjomat.ToList<char>();
+            List<char> regulatmpoint2 = new List<char>();
+
+            Random random = new Random();
+            double los;
+
+            Brush brush = Brushes.Teal;
+            Brush brushtmp = brush;
+
+
+            for (int j = 0; j < powtorzenia; j++)
+                {
+                    foreach (var i in regulatmp)
+                    {
+                    
+                    los = random.NextDouble();
+
+                    if (i == z1.ToCharArray().ElementAt(0) && p1 <= los) regulatmpoint2.AddRange(regula1.ToList<char>());
+
+
+                        if (z2 != "" && p2 <= los)
+                            if (i == z2.ToCharArray().ElementAt(0)) regulatmpoint2.AddRange(regula2.ToList<char>());
+
+
+                    if (z3 != "" && p3 <= los)
+                            if (i == z3.ToCharArray().ElementAt(0)) regulatmpoint2.AddRange(regula3.ToList<char>());
+
+                    if (z4 != "" && p4 <= los)
+                            if (i == z4.ToCharArray().ElementAt(0)) regulatmpoint2.AddRange(regula4.ToList<char>());
+
+                    if (z5 != "" && p5 <= los)
+                            if (i == z5.ToCharArray().ElementAt(0)) regulatmpoint2.AddRange(regula5.ToList<char>());
+
+                    if (z6 != "" && p6 <= los)
+                            if (i == z6.ToCharArray().ElementAt(0)) regulatmpoint2.AddRange(regula6.ToList<char>());
+
+                    string st = i.ToString();
+                        if (st != z1 && st != z2 && st != z3 && st != z4 && st != z5 && st != z6) regulatmpoint2.Add(i);
+                    }
+                    regulatmp = new List<char>();
+                    regulatmp.AddRange(regulatmpoint2);
+                    regulatmpoint2.Clear();
+               }
+
+            
+            double newX = point1.X;
+            double newY = point1.Y;
+            double newKat = kat;
+            double ox= 0, oy= 0;
+            Stack<double> stosX = new Stack<double>();
+            Stack<double> stosY = new Stack<double>();
+            Stack<double> stosKat = new Stack<double>();
+        
+  
+                foreach (char c in regulatmp) 
+                {
+                    switch (c)
+                    {
+                        case 'F':
+                            newX += ox;
+                            newY += oy;
+                            point2 = new Point(newX, newY);
+                            myCanva = LLine(point1, point2, brush);
+                            point1 = point2;
+                            break;
+
+                        case 'f':
+                            newX += ox;
+                            newY += oy;
+                            point2 = new Point(newX, newY);
+                            brushtmp = brush;
+                            brush = Brushes.Transparent;
+                            myCanva = LLine(point1, point2, brush);
+                            brush = brushtmp;
+                            point1 = point2;
+                            break;
+
+                        case '+':
+                            newKat += kat;
+                            ox = Math.Cos(newKat * Math.PI / 180) * krok;
+                            oy = Math.Sin(newKat * Math.PI / 180) * krok;
+
+                            break;
+
+                        case '-':
+                            newKat -= kat;
+                            ox = Math.Cos(newKat * Math.PI / 180) * krok;
+                            oy = Math.Sin(newKat * Math.PI / 180) * krok;
+                            break;
+
+                        case '[':
+                            stosX.Push(newX);
+                            stosY.Push(newY);
+                            stosKat.Push(newKat);
+                            break;
+
+                        case ']':
+                            newX = stosX.Pop();
+                            newY = stosY.Pop();
+                            newKat = stosKat.Pop();
+                            point2 = new Point(newX, newY);
+                            brushtmp = brush;
+                            brush = Brushes.Transparent;
+                            myCanva = LLine(point1, point2, brush);
+                            brush = brushtmp;
+                            point1 = point2;
+                            break;
+
+                        case 'R':
+                            brush = Brushes.Red;
+                            break;
+                        case 'G':
+                            brush = Brushes.Green;
+                            break;
+                        case 'B':
+                            brush = Brushes.DodgerBlue;
+                            break;
+                        case 'T':
+                            brush = Brushes.Teal;
+                            break;
+                        case 'O':
+                            brush = Brushes.Orange;
+                            break;
+                    }
+                }
+            return myCanva;
+        }
+
+        private Canvas LLine(Point point1, Point point2, Brush brush)
+        {
+            Line myLine = new Line();
+
+            myLine.Stroke = brush;
+
+            myLine.X1 = point1.X;
+            myLine.Y1 = point1.Y;
+
+            myLine.X2 = point2.X;
+            myLine.Y2 = point2.Y;
+
+            myLine.HorizontalAlignment = HorizontalAlignment.Center;
+            myLine.VerticalAlignment = VerticalAlignment.Center;
+            
+            myCanva.Children.Add(myLine);
+            myCanva.VerticalAlignment = VerticalAlignment.Center;
+            myCanva.HorizontalAlignment = HorizontalAlignment.Center;
+
+            return myCanva;
+        }
+
+
+    }
+}
---
+++ LSystem/MainWindow.xaml.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LSystem
+{
+
+    #region SYMBOLE
+    /*
+    F- krok w przód o długości d
+    f- krok w przód o długości d bez rysowania
+    +- obrót o określony kąt w prawo
+    -- obrót o określony kąt w lewo
+    [- wrzucenie bierzącego stanu na stos.
+        Zapamiętanie pozycji żółwia, koloru, grubości rysowanej linii itp.
+    ]- bieżącym stanem staje się stan pobrany ze szczytu stosu
+       Żółw nie rysuje przy tym żadnych linii, choć jego pozycja może się zmienić
+    R,G,B,T - kolory pędzla
+
+    Użytkownik podaje symbol startowy, kąt, rozmiar kroku, regułę (z kolorami)
+     */
+    #endregion
+
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myCanva.Children.Clear();
+
+            LInformacje informacje = new LInformacje();
+            informacje.aksjomat = lAksjomat.Text;
+
+            informacje.z1 = z1.Text;
+            informacje.regula1 = lRegula.Text;
+            informacje.p1 = Convert.ToDouble(p1.Text);
+
+
+            if (lRegula2.Text != null && z2.Text != null)
+            {
+                informacje.z2 = z2.Text;
+                informacje.regula2 = lRegula2.Text;
+                informacje.p2 = Convert.ToDouble(p2.Text);
+            }
+
+
+            if (lRegula3.Text != null && z3.Text != null)
+            {
+                informacje.z3 = z3.Text;
+                informacje.regula3 = lRegula3.Text;
+                informacje.p3 = Convert.ToDouble(p3.Text);
+            }
+
+            if (lRegula4.Text != null && z3.Text != null)
+            {
+                informacje.z4 = z4.Text;
+                informacje.regula4 = lRegula4.Text;
+                informacje.p4 = Convert.ToDouble(p4.Text);
+            }
+
+            if (lRegula5.Text != null && z5.Text != null)
+            {
+                informacje.z5 = z5.Text;
+                informacje.regula5 = lRegula5.Text;
+                informacje.p5 = Convert.ToDouble(p5.Text);
+            }
+
+            if (lRegula6.Text != null && z6.Text != null)
+            {
+                informacje.z6 = z6.Text;
+                informacje.regula6 = lRegula6.Text;
+                informacje.p5 = Convert.ToDouble(p5.Text);
+            }
+
+            informacje.kat = Convert.ToDouble(lKat.Text);
+            informacje.krok = Convert.ToDouble(lKrok.Text);
+            informacje.powtorzenia = Convert.ToInt32(lPowtorki.Text);
+
+            Canvas myCanvax = informacje.LRysowanie();
+            
+            var childrenList = myCanvax.Children.Cast<UIElement>().ToArray();
+            foreach (var c in childrenList)
+            {
+                myCanvax.Children.Remove(c);
+                myCanva.Children.Add(c);
+            }
+
+            if (dany == true)
+            {
+                BitmapImage btm = new BitmapImage(new Uri("dany.jpg", UriKind.Relative));
+                Image img = new Image();
+                img.Source = btm;
+                img.Stretch = Stretch.Fill;
+                img.Width = 230;
+                img.Height = 200;
+                img.Margin = new Thickness(350, 0, 0, 0);
+                myCanva.Children.Add(img);
+                dany = false;
+            }
+        }
+
+        private void ButtonW_Click(object sender, RoutedEventArgs e)
+        {
+            myCanva.Children.Clear();
+            CleanForm();
+        }
+
+        private void Canvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var element = sender as UIElement;
+            var position = e.GetPosition(element);
+            var transform = element.RenderTransform as MatrixTransform;
+            var matrix = transform.Matrix;
+            var scale = e.Delta >= 0 ? 1.1 : (1.0 / 1.1);
+
+            matrix.ScaleAtPrepend(scale, scale, position.X, position.Y);
+            transform.Matrix = matrix;
+        }
+
+        private void PrzykladzikW(object sender, System.EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            int selectedInt = comboPrzyklady.SelectedIndex;
+
+            switch (selectedInt)
+            {
+                
+                case 0: // KRZYWA HILBERTA
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 1;
+                    lAksjomat.Text = "X";
+
+                    z1.Text = "X";
+                    lRegula.Text = "-YF+XFX+FY-";
+
+                    z2.Text = "Y";
+                    lRegula2.Text = "+XF-YFY-FX+";
+
+                    lPowtorki.Text = "5";
+                    
+                    lKat.Text = "90";
+                    lKrok.Text = "7";
+                    
+                    break;
+                
+                case 1: // TRÓJKĄT SIERPIŃSKIEGO
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "F+F+F";
+                    lPowtorki.Text = "4";
+                    lRegula.Text = "F+F-F-F+F";
+                    lKat.Text = "120";
+                    lKrok.Text = "10";
+                    z1.Text = "F";
+                    break;
+
+                case 2: // PŁATEK ŚNIEGU KOCH'A
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "F++F++F";
+                    lPowtorki.Text = "3";
+                    lRegula.Text = "F-F++F-F";
+                    lKat.Text = "60";
+                    lKrok.Text = "5";
+                    z1.Text = "F";
+                    break;
+
+                case 3: // KRZYWA PEANO
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 1;
+                    lAksjomat.Text = "X";
+
+                    z1.Text = "X";
+                    lRegula.Text = "XFYFX+F+YFXFY-F-XFYFX";
+
+                    z2.Text = "Y";
+                    lRegula2.Text = "YFXFY-F-XFYFX+F+YFXFY";
+
+                    lPowtorki.Text = "3";
+
+                    lKat.Text = "90";
+                    lKrok.Text = "7";
+                    break;
+
+                case 4: // SMOK HEIGHWAY'A
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 1;
+                    lAksjomat.Text = "FX";
+
+                    z1.Text = "X";
+                    lRegula.Text = "X+YF+";
+
+                    z2.Text = "Y";
+                    lRegula2.Text = "-FX-Y";
+
+                    lPowtorki.Text = "10";
+
+                    lKat.Text = "90";
+                    lKrok.Text = "7";
+                    break;
+
+                case 5: // SMOK LEVY'EGO
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "F";
+                    lPowtorki.Text = "11";
+                    lRegula.Text = "+F--F+";
+                    lKat.Text = "45";
+                    lKrok.Text = "5";
+                    z1.Text = "F";
+
+                    dany = true; // easter egg
+                    break;
+
+                case 6: // PENTADENDRYT
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "F";
+                    lPowtorki.Text = "4";
+                    lRegula.Text = "RF+RF-OF--GF+RF+RF";
+                    lKat.Text = "72";
+                    lKrok.Text = "5";
+                    z1.Text = "F";
+                    break;
+
+                case 7: // GAŁĄŹ 1
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "F";
+                    lPowtorki.Text = "4";
+                    lRegula.Text = "F[+F]F[-F]F";
+                    lKat.Text = "25,7";
+                    lKrok.Text = "4";
+                    z1.Text = "F";
+                    break;
+
+                case 8: // GAŁĄŹ 2
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "----F";
+                    lPowtorki.Text = "3";
+                    lRegula.Text = "FF+[+F-F-F]-[-F+F+F]";
+                    lKat.Text = "22,5";
+                    lKrok.Text = "7";
+                    z1.Text = "F";
+                    break;
+
+                case 9: // Penrose Tiling
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 4;
+                    lAksjomat.Text = "[X]++[X]++[X]++[X]++[X]";
+
+                    z1.Text = "W";
+                    lRegula.Text = "YF++ZF----XF[-YF----WF]++";
+
+                    z2.Text = "X";
+                    lRegula2.Text = "+YF--ZF[---WF--XF]+";
+
+                    z3.Text = "Y";
+                    lRegula3.Text = "-WF++XF[+++YF++ZF]-";
+
+                    z4.Text = "Z";
+                    lRegula4.Text = "--YF++++WF[+ZF++++XF]--XF";
+
+                    z5.Text = "F";
+                    lRegula5.Text = " ";
+
+                    lPowtorki.Text = "3";
+
+                    lKat.Text = "36";
+                    lKrok.Text = "10";
+                    break;
+
+                case 10: // KWADRAT SIERPIŃSKIEGO
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "F+XF+F+XF";
+                    z1.Text = "X";
+                    lRegula.Text = "XF-F+F-XF+F+XF-F+F-X";
+                    lPowtorki.Text = "4";
+                    lKat.Text = "90";
+                    lKrok.Text = "5";
+                    break;
+
+                case 11: // KOŁA
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 0;
+                    lAksjomat.Text = "F+F+F+F";
+                    lPowtorki.Text = "3";
+                    lRegula.Text = "FF+F+F+F+F+F-F";
+                    lKat.Text = "90";
+                    lKrok.Text = "5";
+                    z1.Text = "F";
+                    break;
+
+                case 12: // KRZYWA GOSPERA
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 1;
+                    lAksjomat.Text = "XF";
+
+                    z1.Text = "X";
+                    lRegula.Text = "X+YF++YF-FX--FXFX-YF+";
+
+                    z2.Text = "Y";
+                    lRegula2.Text = "-FX+YFYF++YF+FX--FX-Y";
+
+                    lPowtorki.Text = "4";
+
+                    lKat.Text = "60";
+                    lKrok.Text = "5";
+                    break;
+
+                case 13: // KOLOROWY KRZAK
+                    CleanForm();
+                    comboNumerki.SelectedIndex = 1;
+                    lAksjomat.Text = "X";
+
+                    z1.Text = "X";
+                    lRegula.Text = "RF[G[X]+X]+GF[G+FX]-X";
+
+                    z2.Text = "F";
+                    lRegula2.Text = "FF";
+
+                    lPowtorki.Text = "4";
+
+                    lKat.Text = "-25";
+                    lKrok.Text = "5";
+                    break;
+            }
+
+        }
+
+        private void CleanForm()
+        {
+            foreach (UIElement element in myGrid.Children)
+            {
+                TextBox textbox = element as TextBox;
+                if (textbox != null)
+                {
+                    textbox.Text = String.Empty;
+                }  
+            }
+
+            p1.Text = "0";
+            p2.Text = "0";
+            p3.Text = "0";
+            p4.Text = "0";
+            p5.Text = "0";
+            p6.Text = "0";
+        }
+
+        private bool dany = false;
+    }
+}
